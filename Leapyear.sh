aa0154ed20d3da913837097a676ee6ee9741bd32
rojinroyjo24 <125713908+rojinroyjo24@users.noreply.github.com>
2024-03-21 22:24:41 +1300

    Update and rename Leap year to Leapyear.sh

--- Leapyear.sh
+++ Leapyear.sh
@@ -1,7 +1,8 @@
+<<comment
 QUESTION:
 Write a Shell program to check a leap year
 
-CODE:
+comment
 
 echo "Enter the year (YYYY)"
 read year
@@ -21,11 +22,11 @@ fi
 else
 echo "$year is not a leap year"
 fi
-
+<<comment
 OUTPUT:
 mlm@mlm-H81:~/Desktop/ro/shell$ chmod +x leap.sh
 mlm@mlm-H81:~/Desktop/ro/shell$ ./leap.sh
 Enter the year (YYYY)
 2024
 2024 is a leap year
-
+comment
