55adccaf0fc43ec723cb0d186bcea555bf7174b5
rojinroyjo24 <125713908+rojinroyjo24@users.noreply.github.com>
2024-03-21 22:18:11 +1300

    Update and rename Area and circumference of a circle to area.sh

--- area.sh
+++ area.sh
@@ -1,14 +1,15 @@
+<comment
 QUESTION:
 Write a Shell program to find the area and circumference of a circle
+comment
 
-CODE:
 echo "Enter the radius:"
 read r
 area=`echo 3.14 \* $r \* $r| bc`
 cir=`echo 2 \* 3.14 \* $r| bc `
 echo "Area : $area"
 echo "Circumference : $cir"
-
+<<comment
 OUTPUT:
 mlm@mlm-H81:~/Desktop/ro/shell$ chmod +x circle.sh
 mlm@mlm-H81:~/Desktop/ro/shell$ ./circle.sh
@@ -16,4 +17,5 @@ Enter the radius:
 5
 Area : 78.50
 Circumference : 31.40
+comment
 
