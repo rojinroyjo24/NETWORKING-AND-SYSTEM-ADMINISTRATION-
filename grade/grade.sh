4a2776b8537d6fd310c3ba14eaa12953f7f88973
rojinroyjo24 <125713908+rojinroyjo24@users.noreply.github.com>
2024-03-21 22:15:34 +1300

    Create grade.sh

---
+++ grade/grade.sh
@@ -0,0 +1,26 @@
+INPUT_FILE="marks.txt"
+if [[ ! -f "$INPUT_FILE" ]]; then
+echo "Input file not found!"
+exit 1
+fi
+declare -A grades
+while read line; do
+name=$(echo "$line" | cut -d ',' -f 1)
+grade=$(echo "$line" | cut -d ',' -f 2)
+grades["$name"]=$grade
+done < "$INPUT_FILE"
+for name in "${!grades[@]}"; do
+echo "$name: ${grades[$name]}"
+done
+
+<<comment
+create a file marks.txt
+
+OUTPUT
+
+mlm@mlm-desktop:~/Desktop/Rojin/NW LAB$ chmod +x grade.sh
+mlm@mlm-desktop:~/Desktop/Rojin/NW LAB$ ./grade.sh
+Rojin:90%: Rojin:90%
+visal:98%: visal:98%
+Zaina:80%: Zaina:80%
+comment
